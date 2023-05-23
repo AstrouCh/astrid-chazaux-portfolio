7dfa6da52276a299709124c1a77e5ee776b6cfc8
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-05-23 19:27:00 +0200

    test cursor and design corrrection. Icons don't work

---
+++ app/javascript/controllers/cursor_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="cursor"
+export default class extends Controller {
+  connect() {
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+      $hoverables[i].addEventListener('mouseenter', onMouseHover);
+      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+      TweenMax.to($bigBall, 0.4, {
+        x: e.clientX - 15,
+        y: e.clientY - 15
+      });
+      TweenMax.to($smallBall, 0.1, {
+        x: e.clientX - 5,
+        y: e.clientY - 7
+      });
+    }
+
+    // Hover an element
+    function onMouseHover() {
+      TweenMax.to($bigBall, .3, {
+        scale: 4
+      })
+    }
+    function onMouseHoverOut() {
+      TweenMax.to($bigBall, .3, {
+        scale: 1
+      })
+    }
+  }
+
+}
