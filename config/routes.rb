7f9b36927311db9980fc4efa9063d5f432e2b61b
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-06-06 14:37:16 +0200

    added about alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get "about", to: "pages#about"
+  get "about", to: "pages#about", as: :about
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
