92486e3853a0b0b5ff61c36ef3e0adc8cb5d0241
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-07-12 20:10:54 +0200

    Setting contact form and email notification with Mailer gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about", as: :about
 
-  # root "contact_form#new"
-  resources :contact_form, only: %i[new create]
+  resources :contacts, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
