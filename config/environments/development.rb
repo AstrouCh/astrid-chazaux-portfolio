d4dfca20aa4be67c43a60ea06f5ab9d93a3e10c3
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-08-30 12:48:46 +0200

    About me page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings[:timeout] = 60
   config.action_mailer.smtp_settings = {
     address: 'smtp.office365.com',
     port: 587,
