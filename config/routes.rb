65d03e2d49eeced960371a6d96db963d967dcf44
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-09-01 21:26:05 +0200

    home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "pages#home", as: :home
   get "about", to: "pages#about", as: :about
+  get "myprojects", to: "pages#myprojects", as: :myprojects
 
   resources :contact, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
